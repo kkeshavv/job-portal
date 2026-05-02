ff5f96763d9adbebb12305b7b4c2bc61ba47a6c3
kkeshavv <kkeshavv@github.com>
2026-05-02 10:43:31 +0530

    fix: add missing HttpHeaders import in ResumeControllerTest

--- resume-service/src/test/java/com/capg/resumeservice/controller/ResumeControllerTest.java
+++ resume-service/src/test/java/com/capg/resumeservice/controller/ResumeControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.io.TempDir;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,7 +25,6 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
 @WebMvcTest(ResumeController.class)
 class ResumeControllerTest {
